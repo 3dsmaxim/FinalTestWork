8a5009005c01df82253b7fad63d783693e8a5af3
Maxim Stepanyuk <3dsmaxim@mail.ru>
2022-12-07 02:02:58 +0300

    Написал с горем попалам ввод данных

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,11 +1,25 @@
 ﻿Console.Clear();
 
-Console.WriteLine("Введите строки по оканчанию ввода нажмите q и enter");
-string[] textArray = new string[]{Console.ReadLine(), Console.ReadLine(), Console.ReadLine()};
-Console.WriteLine($"[ {String.Join(", ",textArray)} ]");
+Console.WriteLine("Введите произвольный набор символов");
+Console.WriteLine("Для ввода следующего набора вимволов нажмите enter и повторите ввод");
+Console.WriteLine("для прекращения ввода нажмите enter не вводя никаких символов");
+string enterRow = Console.ReadLine();
+string[] textFyrst = new string[]{};
+string[] textReservide= new string[]{};
+int i = 0;
+while (enterRow != "")
+{
+    textReservide = textFyrst;
+    textFyrst = new string[i+1];
+    
+    for (int j = 0; j < textReservide.Length; j++)
+    {
+        textFyrst[j] = textReservide[j];
+    }
+    textFyrst[i] = enterRow;
+    enterRow = Console.ReadLine();
+    i++;
+     
+}
 
-string enterRowosition = textArray[0];
-int textM = enterRowosition.Length;
-
-Console.WriteLine(enterRowosition);
-Console.WriteLine(textM);
+Console.WriteLine($"[{String.Join(", ",textFyrst)}]");
