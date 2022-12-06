feb69577ba6bda6bd3fd5c2d98df098660d190f7
Maxim Stepanyuk <3dsmaxim@mail.ru>
2022-12-07 02:04:40 +0300

    добавил проверку строк и сборку в массив

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,5 +1,4 @@
 ﻿Console.Clear();
-
 Console.WriteLine("Введите произвольный набор символов");
 Console.WriteLine("Для ввода следующего набора вимволов нажмите enter и повторите ввод");
 Console.WriteLine("для прекращения ввода нажмите enter не вводя никаких символов");
@@ -22,4 +21,38 @@ while (enterRow != "")
      
 }
 
+string[] TextResult(string[] text)
+{
+    string[] result = new string[]{};
+    string[] resultReservide = new string[]{};
+    string check = String.Empty;
+    int m = 0;
+    for (int k = 0; k < text.Length; k++)
+    {
+        check = text[k];
+        if (check.Length <= 3)
+        {
+            resultReservide = result;
+            result = new string[m + 1];
+
+            for (int l = 0; l < resultReservide.Length; l++)
+            {
+                result[l] = resultReservide[l];
+                
+            }
+            result[m] = check;
+            m++;
+        }
+    }
+    return result;
+
+}
+
+
+
+
+Console.Clear();
 Console.WriteLine($"[{String.Join(", ",textFyrst)}]");
+Console.WriteLine();
+Console.WriteLine($"[{String.Join(", ",TextResult(textFyrst))}]");
+Console.WriteLine();
